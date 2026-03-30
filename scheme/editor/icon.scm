94bfe2a3bf54e315001df5efcc80c49ba4ef03df
Dylan Cobb <dylancobb92@gmail.com>
2026-03-30 23:42:56 +0100

    add new icons for welcome pane options

--- scheme/editor/icon.scm
+++ scheme/editor/icon.scm
@@ -9,5 +9,11 @@
 (register-icon 'welcome-icon "tab-icon.svg" 'text.primary)
 (register-icon 'scheme-icon "icon-scheme.svg" 'text.primary)
 
+(register-icon 'new-icon "icon-plus.svg" 'text.primary)
+(register-icon 'open-icon "icon-open.svg" 'text.primary)
+(register-icon 'help-icon "icon-help.svg" 'text.primary)
+(register-icon 'palette-icon "icon-palette.svg" 'text.primary)
+(register-icon 'project-icon "icon-project.svg" 'text.primary)
+
 (defvar default-scale "Default scaling factor for app UI." 1.0)
 (defvar default-buffer-scale "Default scaling factor for buffer text." 1.0)
--- src/display/welcome.c
+++ src/display/welcome.c
@@ -15,13 +15,29 @@ static void XSpacer(void) {
     }){}
 }
 
-static void SuggestionRow(AppState *state, Clay_String label, Clay_String key) {
+static void SuggestionRow(AppState *state, Clay_String label, Clay_String key, const char *icon_name) {
     float font_size = 12 * state->ui.scale_factor;
     TextStyle key_style  = ui_resolve_text_style(state, state->ui.roles.text_key,
                                                   FONT_BUF_NORMAL, font_size);
+    int icon_size = 11.0 * state->ui.scale_factor;
+    SDL_Texture *icon = icon_get(icon_name,   state, icon_size, icon_size);
     CLAY_AUTO_ID({
-        .layout = { .sizing = { .width = CLAY_SIZING_GROW(0) } }
+        .layout = { 
+            .sizing = { .width = CLAY_SIZING_GROW(0) },
+            .padding = { .right = 5.0 * state->ui.scale_factor, .left = 5.0 * state->ui.scale_factor },
+            .childAlignment = { .y = CLAY_ALIGN_Y_CENTER },
+            .childGap = 7.0 * state->ui.scale_factor
+        }
     }) {
+        CLAY_AUTO_ID({
+            .layout = {
+                .sizing = {
+                    .width  = icon_size,
+                    .height = icon_size
+                }
+            },
+            .image = { .imageData = icon },
+        }) {}
         CLAY_TEXT(label, CLAY_TEXT_CONFIG({
             .fontId = FONT_UI_NORMAL,
             .fontSize = font_size,
@@ -98,7 +114,27 @@ void WelcomePane(AppState *state) {
         static char kb_command[64] = ":";
         keymap_where_is_first(state, "tab-new",                  kb_new_tab,  sizeof(kb_new_tab));
         keymap_where_is_first(state, "execute-extended-command", kb_command,  sizeof(kb_command));
-        CLAY(CLAY_ID("Suggestions"), {
+        CLAY(CLAY_ID("Get Started Title"), {
+            .layout = {
+                .sizing = {
+                    .width = CLAY_SIZING_FIXED(400 * state->ui.scale_factor)
+                },
+                .layoutDirection = CLAY_LEFT_TO_RIGHT,
+                .childAlignment = { .y = CLAY_ALIGN_Y_CENTER },
+                .childGap = 5.0 * state->ui.scale_factor,
+            }
+        }) {
+            CLAY_TEXT(CLAY_STRING("GET STARTED"), CLAY_TEXT_CONFIG({
+                .fontId = FONT_UI_NORMAL,
+                .fontSize = 9.0 * state->ui.scale_factor,
+                .textColor = ui_resolve_color(state, state->ui.roles.text_primary)
+            }));
+            CLAY(CLAY_ID_LOCAL("Get Started Divider"), {
+                .layout = { .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_FIXED(1) } },
+                .border = { .width = { .top = 1 }, .color = ui_resolve_color(state, state->ui.roles.border_active)   }
+            }) {}
+        }
+        CLAY(CLAY_ID("Get Started Suggestions"), {
             .layout = {
                 .sizing = {
                     .width = CLAY_SIZING_FIXED(400 * state->ui.scale_factor)
@@ -107,12 +143,12 @@ void WelcomePane(AppState *state) {
                 .childGap = 5.0 * state->ui.scale_factor,
             }
         }) {
-            SuggestionRow(state, CLAY_STRING("Create a new untitled tab"),
-                                 (Clay_String){ .length = strlen(kb_new_tab), .chars = kb_new_tab });
-            SuggestionRow(state, CLAY_STRING("Help commands"),
-                                 (Clay_String){ .length = strlen(kb_help),    .chars = kb_help    });
-            SuggestionRow(state, CLAY_STRING("Invoke command"),
-                                 (Clay_String){ .length = strlen(kb_command), .chars = kb_command });
+            SuggestionRow(state, CLAY_STRING("New File"),
+                                 (Clay_String){ .length = strlen(kb_new_tab), .chars = kb_new_tab }, "new-icon");
+            SuggestionRow(state, CLAY_STRING("Help Commands"),
+                                 (Clay_String){ .length = strlen(kb_help),    .chars = kb_help    }, "help-icon");
+            SuggestionRow(state, CLAY_STRING("Open Command Palette"),
+                                 (Clay_String){ .length = strlen(kb_command), .chars = kb_command }, "palette-icon");
         }
     }
 }
